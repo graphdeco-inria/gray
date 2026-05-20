405c070fcc258121bf84bf7b7482ebe409b6acb9
Yohan Poirier-Ginter <pg.yohan@gmail.com>
2026-05-20 13:08:17 -0400

    fixed disabling sh for rendering

--- gray/raytracer.py
+++ gray/raytracer.py
@@ -347,7 +347,8 @@ class Raytracer(torch.nn.Module):
         gaussians.opacity.copy_(state_dict["opacity"])
         gaussians.channels.copy_(state_dict["channels"])
         gaussians.sh_coeffs_dc.copy_(state_dict["sh_coeffs_dc"])
-        gaussians.sh_coeffs_rest.copy_(state_dict["sh_coeffs_rest"])
+        if config.sh:
+            gaussians.sh_coeffs_rest.copy_(state_dict["sh_coeffs_rest"])
         gaussians.current_sh_degree.copy_(state_dict["current_sh_degree"])
         raytracer.cuda_module.rebuild_bvh()
 
