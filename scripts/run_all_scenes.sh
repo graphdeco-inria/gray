8c39bcd09ebaab9ec99c6d1db403e7aef938dcb0
Yohan Poirier-Ginter <pg.yohan@gmail.com>
2026-05-20 14:45:50 -0400

    simplify run_all_scenes script

--- scripts/run_all_scenes.sh
+++ scripts/run_all_scenes.sh
@@ -3,13 +3,13 @@
 set -e
 
 for scene in treehill flowers garden stump bicycle; do
-    bash run.sh "$1/${scene}/" -i images_4 -s "data/360_v2/${scene}" --eval "${@:2}"
+    bash run.sh "$1/${scene}/" -r 4 -s "data/360_v2/${scene}" --eval "${@:2}"
 done
 
 for scene in kitchen room bonsai counter; do
-    bash run.sh "$1/${scene}/" -i images_2 -s "data/360_v2/${scene}" --eval "${@:2}"
+    bash run.sh "$1/${scene}/" -r 2 -s "data/360_v2/${scene}" --eval "${@:2}"
 done
 
 for path in data/db/* data/tandt/*; do
-    bash run.sh "$1/$(basename "$path")/" -i images_1 -s "$path" --eval "${@:2}"
+    bash run.sh "$1/$(basename "$path")/" -r 1 -s "$path" --eval "${@:2}"
 done
