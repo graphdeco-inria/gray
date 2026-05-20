88c79216cdccec977a92b6dcd1baf5d2d1b3ed9a
Yohan Poirier-Ginter <pg.yohan@gmail.com>
2026-05-20 14:55:01 -0400

    simplify and fix auto scripts

--- scripts/create_all_dense_point_clouds.sh
+++ scripts/create_all_dense_point_clouds.sh
@@ -6,23 +6,23 @@ i=0
 for scene in 360_v2/{bicycle,flowers,garden,treehill,stump,kitchen}; do 
     i=$((i+1))
     echo Creating dense point cloud for scene $scene [${i}/13]
-    python third_party/edgs.py -s data/$scene --roma_model outdoors
+    python third_party/edgs.py -s data/$scene -r 4 --roma_model outdoors
 done
 
 for scene in 360_v2/{bonsai,counter,room}; do
     i=$((i+1))
     echo Creating dense point cloud for scene $scene [${i}/13]
-    python third_party/edgs.py -s data/$scene --roma_model indoors
+    python third_party/edgs.py -s data/$scene -r 2 --roma_model indoors
 done
 
 for scene in tandt/{train,truck}; do
     i=$((i+1))
     echo Creating dense point cloud for scene $scene [${i}/13]
-    python third_party/edgs.py -s data/$scene --roma_model outdoors
+    python third_party/edgs.py -s data/$scene -r 1 --roma_model outdoors
 done
 
 for scene in db/{drjohnson,playroom}; do
     i=$((i+1))
     echo Creating dense point cloud for scene $scene [${i}/13]
-    python third_party/edgs.py -s data/$scene --roma_model indoors
+    python third_party/edgs.py -s data/$scene -r 1 --roma_model indoors
 done
--- scripts/download_all_pretrained_scenes.sh
+++ scripts/download_all_pretrained_scenes.sh
@@ -7,6 +7,6 @@ echo Downloading scenes to \`output/pretrained\`...
 
 for scene in bicycle bonsai counter drjohnson flowers garden kitchen playroom room stump train treehill truck; do 
     curl -fL --progress-bar -o output/pretrained/$scene.zip https://repo-sam.inria.fr/nerphys/gray/pretrained/$scene.zip
-    python -m zipfile -e output/pretrained/$scene.zip output/pretrained/$scene
+    python -m zipfile -e output/pretrained/$scene.zip output/pretrained/
     rm output/pretrained/$scene.zip
 done
